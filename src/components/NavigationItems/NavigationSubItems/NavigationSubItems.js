20242b5aea4753ec6218c6a5407e658753affe20
Chamar <harisamarad@datalink.com.gr>
2018-03-26 18:40:42 +0300

    finallyyyy

--- src/components/NavigationItems/NavigationSubItems/NavigationSubItems.js
+++ src/components/NavigationItems/NavigationSubItems/NavigationSubItems.js
@@ -7,9 +7,8 @@ const navigationSubItems = () => (
     <ul className="NavigationSubItems">
         <NavigationSubItem link="/" >Per User</NavigationSubItem>
         <NavigationSubItem link="/" >Per Expertise</NavigationSubItem>
-        <NavigationSubItem link="/" >Advanced Users</NavigationSubItem>
-        <NavigationSubItem link="/" >empty</NavigationSubItem>
-        <NavigationSubItem link="/" >empty</NavigationSubItem>
+        <NavigationSubItem link="/" >Per Enterprise</NavigationSubItem>
+        
         
     </ul>
 );
